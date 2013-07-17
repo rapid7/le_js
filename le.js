d3c1649f7326e079d3fb7c6a7ac9bc3c1fdf1db7
Chris Mowforth <chris@mowforth.com>
2013-07-17 16:02:36 +0100

    * Add simple querystring-style serialization * Handle multiple overloads:   - simple string literal   - JS object   - interpolated string * prevent further api calls if the first one returns with a 400 code

--- le.js
+++ le.js
@@ -2,9 +2,31 @@ function LogInput(options) {
 
   _token = options.token;
   _that = this;
+  // flag to prevent further invocations on network err
+  _shouldCall = true;
+
+  _serialize = function(obj) {
+    var str = [];
+    for(var p in obj)
+      str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]));
+    return str.join("&");
+  }
 
   _rawLog = function() {
-    _apiCall(_token, arguments[0]);
+    var payload = {};
+    if (arguments.length === 1) {
+      var raw = arguments[0];
+      if (typeof raw === "string") {
+        payload = {event: raw};
+      } else if (typeof raw === "object")
+        payload = raw;
+    } else {
+      // Handle a variadic string overload,
+      // e.g. _rawLog("some text ", x, " ...", 1);
+      interpolated = Array.prototype.slice.call(arguments);
+      payload = {event: interpolated};
+    }
+    _apiCall(_token, _serialize(payload));
   }
 
   this.log = _rawLog;
@@ -18,16 +40,22 @@ function LogInput(options) {
 
     // Obtain a browser-specific XHR object
     _getAjaxObject = function() {
-      if (window.ActiveXObject)
-        return new ActiveXObject('Microsoft.XMLHTTP');
-      else if (window.XMLHttpRequest)
-        return new XMLHttpRequest();
-      return false;
+      if (window.ActiveXObject) {
+        window.XMLHttpRequest = function() {
+          // IE6 compat
+          return new ActiveXObject('Microsoft.XMLHTTP');
+        }
+      }
+      return new XMLHttpRequest();
     }
 
     request = _getAjaxObject();
 
-    if (request) {
+    if (request && _shouldCall) {
+      request.onreadystatechange = function() {
+        if (request.readyState === 4 && request.status === 400)
+          console.warn("Couldn't submit events. Is your token valid?");
+      }
       request.open("POST", _endpoint, true);
       request.setRequestHeader('X-LE-Token', token);
       request.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
@@ -41,8 +69,8 @@ var LE = {
   init: function(options) {
     if (options.token === undefined)
       throw new Error("Token not present.");
-
-    return new LogInput(options);
+    else
+      return new LogInput(options);
   }
 };
 
